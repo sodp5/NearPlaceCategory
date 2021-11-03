78d43c6745a8df36487a3db9982c8fc5d7e72c40
AKMunny <sodp5@naver.com>
2021-11-03 15:56:00 +0900

    이미지를 못찾은 경우 placeholder 대체함

--- app/src/main/java/com/munny/nearplacecategory/bindingadapters/ImageViewBindingAdapters.kt
+++ app/src/main/java/com/munny/nearplacecategory/bindingadapters/ImageViewBindingAdapters.kt
@@ -1,13 +1,12 @@
 package com.munny.nearplacecategory.bindingadapters
 
-import android.view.View
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.CenterCrop
-import com.bumptech.glide.load.resource.bitmap.CircleCrop
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.bumptech.glide.request.RequestOptions
+import com.munny.nearplacecategory.R
 import com.munny.nearplacecategory.extensions.ifFalse
 import com.munny.nearplacecategory.extensions.ifTrue
 import com.munny.nearplacecategory.model.ArticleImage
@@ -25,7 +24,10 @@ fun setArticleImage(iv: ImageView, articleImage: ArticleImage?) {
                 .apply(roundedOption)
                 .into(iv)
         }.ifFalse {
-            iv.visibility = View.GONE
+            Glide.with(iv.context)
+                .load(R.drawable.ic_restaurant_placeholder)
+                .apply(roundedOption)
+                .into(iv)
         }
     }
 }
