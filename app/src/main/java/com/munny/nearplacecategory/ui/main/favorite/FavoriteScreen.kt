665f2b1ebc1ed4cedc63f88a5cd7865c66b6fe47
AKMunny <sodp5@naver.com>
2021-12-26 19:31:23 +0900

    #11 찜목록 없을 때 안내문구 추가

--- app/src/main/java/com/munny/nearplacecategory/ui/main/favorite/FavoriteScreen.kt
+++ app/src/main/java/com/munny/nearplacecategory/ui/main/favorite/FavoriteScreen.kt
@@ -1,14 +1,22 @@
 package com.munny.nearplacecategory.ui.main.favorite
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.munny.nearplacecategory.extensions.ifTrue
 import com.munny.nearplacecategory.model.Place
 import com.munny.nearplacecategory.ui.shared.article.HorizontalArticleItem
+import com.munny.nearplacecategory.values.Colors
 
 @Composable
 fun FavoriteScreen(
@@ -16,10 +24,19 @@ fun FavoriteScreen(
     onItemClickEvent: (Place) -> Unit,
     onLikeClickEvent: (Place) -> Unit
 ) {
-    LazyColumn(
-        modifier = Modifier
-            .padding(horizontal = 16.dp)
-    ) {
+    if (places.isEmpty()) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+        ) {
+            EmptyFavoritePlace(
+                modifier = Modifier
+                    .align(Alignment.Center)
+            )
+        }
+    }
+
+    LazyColumn {
         items(places) { place ->
             HorizontalArticleItem(
                 place = place,
@@ -28,10 +45,23 @@ fun FavoriteScreen(
                 },
                 onLikeClickEvent = {
                     onLikeClickEvent.invoke(place)
-                }
+                },
+                modifier = Modifier
+                    .padding(horizontal = 16.dp)
             )
         }
     }
+}
+
+@Composable
+fun EmptyFavoritePlace(modifier: Modifier = Modifier) {
+    Text(
+        text = "맘에드는 장소에\n하트를 눌러서 모아 보세요!",
+        fontSize = 16.sp,
+        color = Colors.TextGray,
+        textAlign = TextAlign.Center,
+        modifier = modifier
+    )
 }
 
 @Preview
